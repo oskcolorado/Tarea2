05855d2eb483c662118f05063d5454c35ab9c931
luiscolorado <luiscolorado@oskcolorado>
2015-04-23 13:34:46 -0430

    Archivos a utilizar.

---
+++ suiteTest.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-. 
+'''
+Created on 22/04/2015
+
+@author: Luis Colorado   09-11086
+@author: Jose Barrientos 10-10800
+
+'''
+
+
+import unittest
+from mdlaccesscontrol import clsAccessControl
+
+class controlTester(unittest.TestCase):
+    
+    def testEncriptStringEmpty(self):
+        stringEmpty = clsAccessControl()
+        caso = ""
+        casoResultado = stringEmpty.encript(caso)
+        self.assertEqual(casoResultado, "")
+        
+               
+        
