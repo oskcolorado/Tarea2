300a4905ab4d6f6ba8f2ed9be753a35f4f02881e
luiscolorado <luiscolorado@oskcolorado>
2015-04-24 23:24:16 -0430

    Refactorizacion de la definicion "check_password". Funciona correctamente.

--- mdlaccesscontrol.py
+++ mdlaccesscontrol.py
@@ -42,10 +42,14 @@ class clsAccessControl(object):
     def check_password(self, oPassworkEncript, oCheckPassword):
         # Verificar la longitud del password
         olength_password=self.length_password(oCheckPassword)
-        if olength_password>=8 and olength_password<=16: 
-            # uuid es usado para generar numeros random
-            oPassworkEncript, salt = oPassworkEncript.split(':')
-            return oPassworkEncript == hashlib.sha256(salt.encode() + oCheckPassword.encode()).hexdigest()
+        if olength_password>=8 and olength_password<=16 and oPassworkEncript != None:
+            tmp = re.search(self.rePass, oCheckPassword)
+            if (tmp):
+                 
+                # uuid es usado para generar numeros random
+                oPassworkEncript, salt = oPassworkEncript.split(':')
+                if (oPassworkEncript == hashlib.sha256(salt.encode() + oCheckPassword.encode()).hexdigest()):
+                    return True
         else:
             print('El Password no posee la cantidad de caracteres requerida')
             return False
